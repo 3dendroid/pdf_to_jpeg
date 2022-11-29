df5788ef0ac98f1b15d82988a8f00ba2c35c2f36
GIGACHAD <denkingbad@gmail.com>
2022-11-29 12:12:16 +0700

    fixed and working code

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import os
 from pdf2image import convert_from_path
 
 poppler_path = r'C:\Program Files (x86)\poppler-0.68.0\bin'
-pages = convert_from_path(r'C:\Users\GIGACHAD\PycharmProjects\pdf_to_jpeg\pdf\ticket.pdf', 500)
+pdf_path = r'C:\Users\GIGACHAD\PycharmProjects\pdf_to_jpeg\pdf\ticket.pdf'
 
-pages = convert_from_path(pdf_path=pages, poppler_path=poppler_path)
+pages = convert_from_path(pdf_path=pdf_path, poppler_path=poppler_path)
 saving_folder = r'C:\Users\GIGACHAD\PycharmProjects\pdf_to_jpeg\jpg'
 
 i = 1
 
 for page in pages:
-    img_name = f'img-{i}.jpg'
-    page.save(os.path.join(saving_folder, img_name), "JPG")
+    img_name = f'img-{i}.jpeg'
+    page.save(os.path.join(saving_folder, img_name), "JPEG")
     i += 1
